8b60e16ccc400195559998984a2a62d8e3e75a03
alpayH <alpayhesenli@gmail.com>
2025-04-04 16:09:50 +0200

    Best Model (24H_True)

--- Utils/config.py
+++ Utils/config.py
@@ -5,14 +5,14 @@ import torch
 # Training configuration
 # -----------------------------------------
 
-FILES_USED = '1' # '1' for 12h, '2' for 24h, '3' for both files
+FILES_USED = '2' # '1' for 12h, '2' for 24h, '3' for both files
 TARGET_COLUMN = 'Patient Outcome'
 PREPROCESS = False
 ALL_FEATURES = True # If False, only keep features in FEATURES_TO_KEEP
 #FEATURES_TO_KEEP_12 = ['BSR','abs(renyi)','fhtife2','beta_tot']
 #FEATURES_TO_KEEP_24 = ['SkewAM','abs(shan)','KurtAM','skewness','beta_theta','BSR','spindle_theta','meanAM','fhtife3','alpha','beta_tot','Complexity','spindle','alpha_delta']
 FEATURES_TO_KEEP = None
-MODEL_NAME = "RF_12h"
+MODEL_NAME = "RF_24h"
 TRAINING = True
 
 
